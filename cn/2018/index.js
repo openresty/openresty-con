62018ff35f06e1e6f40b0d1a857115ed8c62bcf0
monkindey <monkindey@163.com>
2018-10-17 23:01:20 +0800

    recalculation

--- cn/2018/index.js
+++ cn/2018/index.js
@@ -166,7 +166,8 @@
   // 记录前一个被点击的头像
   var preClickedAvatar = null;
   var preClickedIndex = -1;
-  var screen = 875;
+  // 890 - (140 - 100)
+  var screen = 850;
   var whereIsChun = 3;
   var whereIsOther = 10;
   var whereIsPerson = [whereIsChun, whereIsOther];
